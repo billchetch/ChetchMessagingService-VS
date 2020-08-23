07a0138c6dc54773d8118e1317d7ede137421b21
billchetch <45006134+billchetch@users.noreply.github.com>
2020-08-23 23:42:36 +0800

    Add project files.

---
+++ ChetchMessagingService/CMServerInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chetch.Services;
+
+
+namespace ChetchMessagingService
+{
+    [RunInstaller(true)]
+    public class CMServerInstaller : ServiceInstaller
+    {
+        public CMServerInstaller() : base("ChetchMessagingService", 
+                                    "Chetch Messaging Service", 
+                                    "Runs an instance of the chetch messaging server")
+        {
+            //empty
+        }
+    }
+}
